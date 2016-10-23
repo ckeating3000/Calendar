fcf464476e48933f7ea4b05574d4c1bcb93bf28b
Kate Harline <kharline@wustl.edu>
2016-10-23 15:02:37 -0500

    al/kg

--- calendar.php
+++ calendar.php
@@ -117,6 +117,7 @@
             
             cal+= '</table>';
             document.getElementById("calSpot").innerHTML = cal;
+            document.getElementById("date").addEventListener("click", viewEvents, false);
             }
             
             //first load
@@ -161,6 +162,7 @@
             }
             cal+= '</table>';
             document.getElementById("calSpot").innerHTML = cal;
+            document.getElementById("date").addEventListener("click", viewEvents, false);
             }
         </script>
     </head>
@@ -184,26 +186,26 @@
         <div id="viewEvents" title="Events">
             View the events on this day
                 <?php
-                session_start();
-                //display events on the date selected
-                require 'database.php';
-                $get_events = $mysqli->prepare("select time, event_text from events where date=?, username=?");
-                if(!$get_events){
-                    printf("Query Prep Failed: %s\n", $mysqli->error);
-                    exit;
-                }
-                $get_events->bind_param('is', $date, $user);
-                $get_events->execute();
-                $get_events->bind_result($time, $event);
-                 
-                while($get_events->fetch()){
-					//nonusers can only see comments from other users
-                        printf(" %s %u",
-                        "Event title: ".htmlspecialchars($event),
-                        "Event time: ".htmlspecialchars($time)
-                    );
-                } 
-                $get_events->close();
+//                session_start();
+//                //display events on the date selected
+//                require 'database.php';
+//                $get_events = $mysqli->prepare("select time, event_text from events where date=?, username=?");
+//                if(!$get_events){
+//                    printf("Query Prep Failed: %s\n", $mysqli->error);
+//                    exit;
+//                }
+//                $get_events->bind_param('is', $date, $user);
+//                $get_events->execute();
+//                $get_events->bind_result($time, $event);
+//                 
+//                while($get_events->fetch()){
+//					//nonusers can only see comments from other users
+//                        printf(" %s %u",
+//                        "Event title: ".htmlspecialchars($event),
+//                        "Event time: ".htmlspecialchars($time)
+//                    );
+//                } 
+//                $get_events->close();
                 ?>
                 
         </div>
@@ -212,7 +214,7 @@
             function start() {
                 firstCalendar();
                 document.getElementById("eventAdder").addEventListener("click", addEvent, false);
-                document.getElementById("date").addEventListener("click", viewEvents, false);
+
               }
               window.onload = start;
              //document.addEventListener("DOMContentLoaded", printCalendar, false);
